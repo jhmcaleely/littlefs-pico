472d64c9b30a52c471892955248132e0aba86ac7
John McAleely <john@mcaleely.com>
2025-01-30 18:33:49 +0000

    Documentaton for README and pico_lfs_hal.c

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # littlefs-pico
 
 A repo to build the example code found in [littlefs/README.md](https://github.com/littlefs-project/littlefs/blob/0494ce7169f06a734a7bd7585f49a9fa91fa7318/README.md) for the Raspberry Pi Pico, using the vscode plugin and Pico SDK.
+
+After installing the Raspberry Pi Pico SDK extensions to VSCode, opening this folder should present build and run options.
+
+Alternatively, if you have the Pico SDK installed:
+
+'''
+% cmake .
+% cmake --build .
+'''
+
+will create 'littlefs-pico.uf2' that can be flashed onto a pico as usual. By default, serial output (from printf, etc) will be available over the USB and uart ports on the pico.
+
+## main.c
+
+The example code that formats a littlefs drive, and counts reboots in a file.
+
+## pico_lfs_hal.c and pico_lfs_hal.h
+
+An implementation of the littelfs block device that uses the Pico C SDK (tested on 2.1) to read/write/erase blocks to the built in flash on a Pico.
+
+## pico_flash_fs.h
+
+A definition of a single flash area to use as a filesystem. The range is at the opposite end of flash to that used by code, so that it is easy to reflash code without clobbering the flash drive.
+
+## Tools
+
+I found '[flash_nuke.utf2](https://www.raspberrypi.com/documentation/microcontrollers/pico-series.html#resetting-flash-memory)' useful to reset the pico's flash device for testing.
--- pico_lfs_hal.c
+++ pico_lfs_hal.c
@@ -10,6 +10,9 @@
 // We need an offset in bytes for erase and program operations.
 #define FLASHFS_FLASH_OFFSET ((const size_t)(FLASHFS_BASE_ADDR - PICO_FLASH_BASE_ADDR))
 
+/*
+ * Read from the flash device. Pico's flash is memory mapped, so memcpy will work well
+ */
 int pico_read_flash_block(const struct lfs_config *c, 
                           lfs_block_t block,
                           lfs_off_t off, 
@@ -19,6 +22,15 @@ int pico_read_flash_block(const struct lfs_config *c,
     memcpy(buffer, FLASHFS_BASE_ADDR + block * PICO_ERASE_PAGE_SIZE + off, size);
     return LFS_ERR_OK;
 }
+
+/*
+ * The Pico SDK provides flash_safe_execute which can be used to wrap write (erase/program)
+ * operations to the flash device. The implementation varies, depending on your target 
+ * configuration. It will disable interupts, and pause code execution on core 1 if needed.
+ * 
+ * Using it needs the underlying functions to be passed parameters through a single data
+ * pointer, hence the need for a couple of structures to pass them.
+ */
 
 struct prog_param {
     uint32_t flash_offs;
@@ -31,6 +43,9 @@ static void call_flash_range_program(void* param) {
     flash_range_program(p->flash_offs, p->data, p->count);
 }
 
+/*
+ * Write (program) a block of data to the flash device. 
+ */
 int pico_prog_flash_block(const struct lfs_config *c, 
                           lfs_block_t block,
                           lfs_off_t off, 
@@ -61,6 +76,9 @@ static void call_flash_range_erase(void* param) {
     flash_range_erase(p->offset, p->size);
 }
 
+/*
+ * Erase a block of flash, in preparation for future writes
+ */
 int pico_erase_flash_block(const struct lfs_config *c, lfs_block_t block) {
     
     struct erase_param p;
@@ -76,6 +94,12 @@ int pico_erase_flash_block(const struct lfs_config *c, lfs_block_t block) {
     }
 }
 
+/*
+ * The need for this function is unclear. As of writing, it has little 
+ * documentation. If we access flash with caching disabled (via XIP_NOCACHE_NOALLOC_BASE)
+ * I imagine it may be doing nothing. No provision for wrapping it in flash_safe_execute
+ * appears to exist.
+ */
 int pico_sync_flash_block(const struct lfs_config *c) {
     flash_flush_cache();
     return LFS_ERR_OK;
